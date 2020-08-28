5488e55c9a228c51ebf43d34eaf9a4cb5d01f0ea
mridle582 <mr.idle582@gmail.com>
2020-08-27 20:48:26 -0400

    Removed redundent shortcuts

--- config.h
+++ config.h
@@ -66,19 +66,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-c", NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *bravecmd[] = {"brave", NULL};
-static const char *spotifycmd[] = {"spotify", NULL};
-static const char *discordcmd[] = {"discord", NULL};
-static const char *rangercmd[] = {"st", "-e", "ranger", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v =  discordcmd} },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_w,      spawn,          {.v = bravecmd}},
-	{ MODKEY,                       XK_s,      spawn,          {.v = spotifycmd}},
-    { MODKEY,                       XK_r,      spawn,          {.v = rangercmd}},
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
